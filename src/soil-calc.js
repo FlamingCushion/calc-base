5ab538dd98750f4ff73f523b15c32c9825a7a9c9
Martyn Gosling <flamingcushion@gmail.com>
2018-01-15 23:04:31 +0000

    dev - completion of calculators and styling

--- src/soil-calc.js
+++ src/soil-calc.js
@@ -1,9 +1,9 @@
-import React, {Component} from 'react';
+import React from 'react';
 
 // App Components
-import Range from './inputs';
+import Range from './inputs.js';
 
-export default class SeedCalculator extends Component {
+export default class SoilCalculator extends React.Component {
     constructor(props) {
         super(props);
         this.state = this.defaultState();
@@ -12,67 +12,65 @@ export default class SeedCalculator extends Component {
     render() {
         return (
             <div className='calculator'>
-                <h3>Grass Seed Calculator</h3>
-                <div className='seed-result-container'></div>
+                <h3>Top Soil Calculator</h3>
+                <div>
+                    <p>
+                        Please enter the area measurements (in metres) and the depth of soil (in centermeters).
+                    </p>
+                </div>
 
-                <div className='seed-input'>
-                    <div className='input-fields'>
-                        <div className='input-group'>
-                            <span className='label'>Width (m)</span>
+                <div className='calculator__input'>
+                    <div className='form-elements'>
+                        <div className='form-group'>
+                            <span className='label'>Width of Area (m)</span>
                             <Range min={0.5} value={ this.state.width } onChange={ this.widthChange } />
                         </div>
-                        
-                        <div className='input-group'>
-                            <span className='label'>Area (m^2)</span>
-                            <Range min={10} max={10000000} value={ this.state.area } onChange={ this.areaChange } />
+
+                        <div className='form-group'>
+                            <span className='label'>Length of Area (m)</span>
+                            <Range min={0.5} max={10000000} value={ this.state.length } onChange={ this.lengthChange } />
                         </div>
 
-                        <div className='input-group'>
-                            <span className='label'>depth (kg/m^2)</span>
-                            <Range min={0.02} value={ this.state.seeding } onChange={ this.seedingChange } />
+                        <div className='form-group'>
+                            <span className='label'>Depth of Soil (cm)</span>
+                            <Range min={0.02} value={ this.state.depth } onChange={ this.depthChange } />
                         </div>
                     </div>
                 </div>
-                
-                <div className='seed-result-number'>
-                    Grass Seed Needed = { this.state.seed } kg
+
+                <div className='calculator__results'>
+                    <span>Volume of Soil Needed: </span>
+                    <span>{ this.state.soil.toFixed(2) } m<sup>3</sup></span>
                 </div>
             </div>
         );
     }
-    
-þ☼ƒ
 
-    areaChange = (area) => {
-        this.setState({ area: area }, this.setSeed);
+    widthChange = (width) => {
+    this.setState({ width: width }, this.setSoil);
+    }
+
+    lengthChange = (length) => {
+        this.setState({ length: length }, this.setSoil);
     }
 
     depthChange = (depth) => {
-        this.setState({ depth: depth }, this.setLiner);
+        this.setState({ depth: depth }, this.setSoil);
     }
-    
+
     reset = () => { this.setState(this.defaultState()); }
 
     defaultState = () => {
         return {
-            area: 100,
-            depth: 0.1,
-            seed: 10,
-            seedClass: 'Normal Weight'
+            width: 10,
+            length: 10,
+            depth: 10,
+            soil: 10,
         };
     }
 
     setSoil = () => {
-        let soil = (this.state.area * this.state.depth );
-        this.setState({ seed: seed });
-    }
-
-    getStyles = () => {
-        return {
-            underweight: { color: '#FFFFFF' },
-            normal: { color: '#F9F68D' },
-            overweight: { color: '#FED88B' },
-            obese: { color: '#FF5411' }
-        }
+        let soil = (this.state.length * this.state.width * (this.state.depth / 100));
+        this.setState({ soil: soil });
     }
 }
